d8ff41de853b83762b22ae5bb417874b56c64e98
aleqze <santosal2018@gmail.com>
2025-02-06 13:41:41 -0800

    doubly ll palindrome checker

---
+++ Section 9 -DLL LeetCode/palindrome_checker.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    # WRITE IS_PALINDROME METHOD HERE #
+    def is_palindrome(self):
+        if self.length == 0:
+            return True
+        if self.length == 1:
+            return True
+        for _ in range(self.length // 2 + 1):
+            start = self.head
+            end = self.tail
+            if start.value != end.value:
+                return False
+            else:
+                start = start.next
+                end = end.next
+        return True
+                
+
+
+
+
+my_dll_1 = DoublyLinkedList(1)
+my_dll_1.append(2)
+my_dll_1.append(3)
+my_dll_1.append(2)
+my_dll_1.append(1)
+
+print('my_dll_1 is_palindrome:')
+print( my_dll_1.is_palindrome() )
+
+
+my_dll_2 = DoublyLinkedList(1)
+my_dll_2.append(2)
+my_dll_2.append(3)
+
+print('\nmy_dll_2 is_palindrome:')
+print( my_dll_2.is_palindrome() )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    my_dll_1 is_palindrome:
+    True
+
+    my_dll_2 is_palindrome:
+    False
+
+"""
+
